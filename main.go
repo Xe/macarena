1ebe6b4030d629620bf04dfc6215bb4bf4ef0556
Sam Dodrill <xena@yolo-swag.com>
2015-02-18 02:05:47 -0800

    update main

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"flag"
 	"log"
 
+	"github.com/Xe/macarena/bot"
 	"github.com/Xe/macarena/config"
+	"github.com/thoj/go-ircevent"
 )
 
 var (
 	cfgFname = flag.String("conf", "./config.json", "config file to use")
+
+	parent chan *irc.Event
+	bots   []bot.Bot
 )
+
+func init() {
+	parent = make(chan bot.Event)
+}
 
 func main() {
 	flag.Parse()
